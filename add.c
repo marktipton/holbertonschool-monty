1301d7773a07600796cefcbc8e6c5e50feed3cd5
marktipton <6675@holbertonstudent.com>
2023-08-13 14:47:42 -0700

    added push function

--- add.c
+++ add.c
@@ -9,5 +9,15 @@
  */
 void add(stack_t **top, unsigned int line_number)
 {
-	(void)top, (void)line_number;
+	int ultimate, penultimate;
+
+	if (*top == NULL || (*top)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	ultimate = (*top)->n;
+	penultimate = (*top)->next->n;
+	(*top)->next->n = ultimate + penultimate;
+	pop(top, line_number);
 }
