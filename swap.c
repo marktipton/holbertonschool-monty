aca5924a20e8aff05c5d29afbb938a62d2a718a3
marktipton <6675@holbertonstudent.com>
2023-08-13 14:39:24 -0700

    added swap function

--- swap.c
+++ swap.c
@@ -8,5 +8,16 @@
  */
 void swap(stack_t **top, unsigned int line_number)
 {
-	(void)top, (void)line_number;
+	stack_t *temp;
+
+	if (*top == NULL || (*top)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *top;
+	*top = (*top)->next;
+	temp->next = (*top)->next;
+	(*top)->next = temp;
+
 }
